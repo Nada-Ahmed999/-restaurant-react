c16b197360cdb0c7203f2c940bea00bd72d95b98
Nada Ahmed <nada.ahmed.291021@gmail.com>
2025-11-30 18:45:48 +0200

    Update img className for responsive styling

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -20,7 +20,7 @@ export default function Home() {
         <source src="../../public/video/offer.mp4" type="video/mp4" className='position-absolute' />
       </video> */}
 
-      <img src="https://as2.ftcdn.net/v2/jpg/04/76/57/27/1000_F_476572792_zMwqHpmGal1fzh0tDJ3onkLo88IjgNbL.jpg" alt="" className='position-absolute bg-video' />
+      <img src="https://as2.ftcdn.net/v2/jpg/04/76/57/27/1000_F_476572792_zMwqHpmGal1fzh0tDJ3onkLo88IjgNbL.jpg" alt="" className={`position-absolute bg-video ${size<992?'py-5 my-5':''}`}  />
 
   </div>
   <div className="content lobster-two-bold position-absolute end-0 p-5 w-75 ">
