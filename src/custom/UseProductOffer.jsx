2a486a95f8d6a0389d9122d332fd2eaada1b6e3c
Nada Ahmed <nada.ahmed.291021@gmail.com>
2025-11-30 18:00:27 +0200

    change image

--- src/custom/UseProductOffer.jsx
+++ src/custom/UseProductOffer.jsx
@@ -26,7 +26,8 @@ export default function UseProductOffer() {
    return <div className='pt-1' key={index}>
      <div className={`${size < 727 ?'w-100':'w-50'} mt-3 shadow border-3 border-warning border  m-auto rounded-5 py-4 position-relative d-flex justify-content-between`} style={{backgroundColor:'#f2eeeac2'}}>
             <div className='border-2 border-warning border-end w-60'>
-               <img src="../../public/ofeer.png" alt="50offer" width={70} className='position-absolute end-0 bottom-50  mb-4'  />
+               {/* <img src="../../public/ofeer.png" alt="50offer" width={70} className='position-absolute end-0 bottom-50  mb-4'  /> */}
+               <img src="https://img.freepik.com/premium-psd/special-offer-50-percent-discount-3d-rendering-sale-banner_563397-9.jpg" alt="50offer" width={70} className='position-absolute end-0 bottom-50  mb-4 rounded-circle'  />
                <img src={orderItem.image} alt="" width={120} height={120} className={` ${size<727?"mt-3 end-0 top-50":"end-100"} position-absolute top-0 bg-light rounded-circle  me-5 `}   /> 
                  <img src={JSON.parse(localStorage[`${orderItem.title} drink`]).strDrinkThumb} alt="drink" width={80} height={80} className={`${size <727?'end-0':'end-100'} rounded-circle position-absolute  me-3 top-50 border-4 border-light border`} />
                  <h3 className='mx-4 text-orange px-5 fw-bold'>{`${orderItem.title} + ${JSON.parse(localStorage[`${orderItem.title} drink`]).strDrink}`}
